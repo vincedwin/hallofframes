1cd555d7ffeaebc0767eee1e0639a7a2c125fdc2
vincedwin <52823998+vincedwin@users.noreply.github.com>
2022-09-25 20:41:03 +0800

    initial-drop

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className='font-extrabold'>
+        halls of frames
+    </div>
+  )
+}
+
+export default Home
